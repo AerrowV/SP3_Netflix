a0ead982673c40c1afe766a7e35b6eaa5712c9b5
AerrowV <businessasimk@gmail.com>
2024-04-19 12:35:08 +0200

    zana er en bandit

--- src/TextUI.java
+++ src/TextUI.java
@@ -1,23 +1,11 @@
 public class TextUI {
 
-        //metode til at kalde en besked
-    public String DisplayMsg(String msg){
-        return msg;
-    }
-
-    public String prompt(String msg){
-
-        displayMsg(msg);
-        String input = scan.nextLine();
-        return input;
-    }
-
-    public void displayMsg(String msg){
-        System.out.println("\n***************");
+    //metode til at kalde en besked
+    public void displayMsg(String msg) {
         System.out.println(msg);
-        System.out.println("***************\n");
-    }
 
-    
+    }
 
 }
+
+
