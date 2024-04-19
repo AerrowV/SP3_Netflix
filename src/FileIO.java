0f72ecf3e11f4df9ce70c68d6a71a2c3fdd35096
Maximitas <Lassetimm@outlook.dk>
2024-04-19 12:50:23 +0200

    FileIO Changes only part two

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,23 +1,40 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileIO {
     private String dataFile = "UserAccountData.txt";
 
-    public ArrayList<String> readFileData(String path){
+    public void readFileData(String path, int textUnits){
+        String [] data = new String[textUnits];
+        File file = new File(path);
+
         try {
             File textFile = new File(path);
             Scanner scan = new Scanner(textFile);
             while (scan.hasNextLine()) {
+                Scanner scan = new Scanner(file);
 
+                while (scan.hasNextLine()) {
+                    for(int i = 0; i < textUnits; i++){
+                        String line = scan.nextLine();
+                        data[i] = line;
+                        data = line.split(";", 4);
+
+                        for (int y = 0; y < data.length; y++) {
+                            System.out.println(data[y]);
+                        }
+                        System.out.println("\n");
+                    }
+                }
+                scan.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
             }
-            scan.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
         }
-    }
 
     public void readUserData(ArrayList<Users> user, String path) {
         /*
@@ -37,13 +54,11 @@ public class FileIO {
     }
 
 
-
+/*
     public void saveUserData(ArrayList<Users> user, String path){
 
-
-
     }
-
+*/
     public boolean checkCredentials(String email, String password) {
         try (Scanner scanner = new Scanner(new FileReader(dataFile))) {
             while (scanner.hasNextLine()) {
