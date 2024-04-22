b3ec9701187d2d1a467222935de19d5be5127f5f
AerrowV <businessasimk@gmail.com>
2024-04-22 20:08:12 +0200

    Current version

--- src/Season.java
+++ src/Season.java
@@ -7,12 +7,11 @@ public class Season {
 
     public Season(String seasonInfo) {
         String[] parts = seasonInfo.split("-Episode ");
-        if (parts.length == 2) {
             this.seasonNumber = parts[0].trim();
             int episodeCount = Integer.parseInt(parts[1].trim());
             for (int i = 1; i <= episodeCount; i++) {
                 episodes.add(new Episode(i));
-            }
+
         }
     }
 
